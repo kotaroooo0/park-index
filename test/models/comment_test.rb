085d50b25847a180563661e67e4e3662f4bb0bff
k33asby <k33asby@gmail.com>
2018-02-03 21:39:55 +0900

    Add comment model test but not work

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+
+  # def setup
+  #   @comment = comments(:valid_comment)
   # end
+  #
+  # test "should be valid" do
+  #   assert @comment.valid?
+  # end
+  #
+  # test "body should be presence" do
+  #   @comment.body = ""
+  #   assert_not @comment.valid?
+  # end
+
 end
