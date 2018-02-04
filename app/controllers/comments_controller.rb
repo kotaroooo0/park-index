2c9a8aa456c00c88b802dd4b1e1c330ff57b5de0
k33asby <k33asby@gmail.com>
2018-02-04 16:56:02 +0900

    Design park show view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,15 +2,15 @@ class CommentsController < ApplicationController
 
   before_action :logged_in_user?, only:[:create, :destroy]
   before_action :correct_user, only: :destroy
-
-
+  
   def create
     @comment = current_user.comments.build(comment_params)
     if @comment.save
       flash[:success] = "コメントを投稿しました。"
       redirect_to request.referrer || root_url
     else
-      render root_path
+      flash[:danger] = "コメントの投稿に失敗しました。"
+      redirect_to @comment.park
     end
   end
 
