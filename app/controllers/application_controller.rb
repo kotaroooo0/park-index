4140c8949335c8378d4db8aa84a3c324dc7d63b4
k33asby <k33asby@gmail.com>
2018-02-09 22:46:14 +0900

    Refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,13 @@ class ApplicationController < ActionController::Base
     redirect_to login_url
   end
 
+  def correct_user?
+    return if current_user.id == session[:user_id]
+    store_location
+    flash[:info] = "正しいユーザーではありません"
+    redirect_to root_url
+  end
+
   def profile_image(img)
     img.blank? ? "profile_pic.jpeg" : img
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
   before_action :logged_in_user?, only:[:create, :destroy]
-  before_action :correct_user, only: :destroy
+  before_action :correct_user?, only: :destroy
 
   def create
     @comment = current_user.comments.build(comment_params)
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @comment.destroy
+    Comment.find(params[:id]).destroy
     flash[:success] = "コメントを削除しました。"
     redirect_to request.referrer || root_url
   end
@@ -25,5 +25,6 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:body, :park_id)
   end
+
 
 end
